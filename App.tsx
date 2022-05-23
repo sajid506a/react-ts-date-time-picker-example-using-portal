0703c5d42806a4fe58b47b1d23b4fb838d0384d4
sajid506a <sajid87a@gmail.com>
2022-05-23 21:19:24 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import DatePicker from 'react-multi-date-picker';
+import './style.css';
+function Example() {
+  const [value, setValue] = React.useState(new Date());
+
+  return(
+    <div
+    style={{
+      position: "fixed",
+      overflow: "auto",
+      display: "flex",
+      justifyContent: "space-around",
+      height: "200px",
+      maxWidth: "500px",
+      backgroundColor: "lightgray",
+      borderRadius: "5px",
+    }}
+  >
+    <div>
+      <h5>Default:</h5>
+      <DatePicker />
+    </div>
+    <div>
+      <h5>Portal:</h5>
+      <DatePicker multiple portal  />
+    </div>
+  </div>
+  );
+}
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <h1>Date picker</h1>
+      <Example />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-4sxh5d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-4sxh5d)
